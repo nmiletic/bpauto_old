3e5304e23d6d6432dd3e5a2671379ae0a19a2ea9
Nemanja Miletic <nemanja@nemanjamiletic.com>
2017-08-02 12:17:41 +0200

    first commit

---
+++ bp.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+"""Library for generating BreakingPoint TCL commands."""
+
+from io import StringIO
+import yaml
+from itertools import cycle
+
+class TCLFiles(object):
+    def __init__(self):
+        self._createbuf = StringIO()
+        self._deletebuf = StringIO()
+
+    def pcreate(self, command):
+        print(command, file=self._createbuf)
+
+    def pdelete(self, command):
+        print(command, file=self._deletebuf)
+
+    def save_create(self, prefix):
+        filename = prefix + 'create.tcl'
+        with open(filename, 'w') as createfile:
+            createfile.write(self._createbuf.getvalue())
+
+
+class BreakingPoint(object):
+    def __init__(self, *, prefix=None):
+        self._prefix = prefix
+        self.tfiles = TCLFiles()
+        self._network = None
+
+    def connect(self, *, hostname=None, login='admin', password='admin'):
+        command = ('set bps [bps::connect "{HOSTNAME}" "{LOGIN}" "{PASSWORD}" '
+                   '-onclose exit -shortcuts true]')
+        self.tfiles.pcreate(command.format(HOSTNAME=hostname, LOGIN=login, PASSWORD=password))
+
+    def save(self):
+        self.tfiles.save_create(self._prefix)
+
+    def create_network(self, name='NN'):
+        command = ('set n [$bps createNetwork -name "{PREFIX}{NAME}"]')
+        self.tfiles.pcreate(command.format(NAME=name, PREFIX=self._prefix))
+        self._network = Network(self.tfiles, self._prefix + name)
+        command = ('$n begin')
+        self.tfiles.pcreate(command)
+        return self._network
+
+
+class Network(object):
+    def __init__(self, tfiles, name):
+        self._name = name
+        self.tfiles = tfiles
+        self._interfaces = []
+        self._containers = []
+        self._ip_static_hosts = []
+        self._paths = []
+
+    def add_interface(self, name, number, mac_address):
+        command = '$n add interface -number {NUMBER} -id "{ID}" -mac_address "{MAC}" -duplicate_mac_address 1'
+        self.tfiles.pcreate(command.format(NUMBER=number,
+                                           ID=name,
+                                           MAC=mac_address))
+        self._interfaces.append(name)
+        self._containers.append(name)
+
+    def add_vlan(self, name, container, vlan, mac_address):
+        command = '$n add vlan -id {ID} -default_container "{CONTAINER}" -inner_vlan {VLAN} -mac_address "{MAC}" -duplicate_mac_address 1'
+        self.tfiles.pcreate(command.format(ID=name, CONTAINER=container, VLAN=vlan, MAC=mac_address))
+        self._containers.append(name)
+
+    def add_ip_router(self, name, container, ip_address, gateway, netmask):
+        command = ('$n add ip_router -id "{NAME}" '
+                   '-default_container "{CONTAINER}" -ip_address "{IP}" '
+                   '-gateway_ip_address "{GATEWAY}" -netmask {NETMASK}')
+        self.tfiles.pcreate(command.format(NAME=name,
+                                           CONTAINER=container,
+                                           IP=ip_address,
+                                           NETMASK=netmask,
+                                           GATEWAY=gateway))
+        self._containers.append(name)
+
+    def add_ip_static_hosts(self, name, tag, container, ip_address, count, netmask, gateway):
+        command = ('$n add ip_static_hosts -id "{NAME}" '
+                   '-tags [list "{NAME}" "{TAG}"] '
+                   '-default_container "{CONTAINER}" -ip_address "{IP}" '
+                   '-count {COUNT} -netmask {NETMASK} '
+                   '-gateway_ip_address "{GATEWAY}"')
+        self.tfiles.pcreate(command.format(NAME=name,
+                                           TAG=tag,
+                                           CONTAINER=container,
+                                           IP=ip_address,
+                                           COUNT=count,
+                                           NETMASK=netmask,
+                                           GATEWAY=gateway))
+        self._ip_static_hosts.append(name)
+
+    def get_interface_group(self, prefix):
+        return [i for i in self._interfaces if (i.startswith(prefix))]
+
+    def get_container_group(self, prefix):
+        return [i for i in self._containers if (i.startswith(prefix))]
+
+    def get_ip_static_hosts_group(self, prefix):
+        return [i for i in self._ip_static_hosts if (i.startswith(prefix))]
+
+    def add_path(self, clientid, serverid):
+        command = '$n addPath "{CLI_ID}" "{SER_ID}"'
+        self.tfiles.pcreate(command.format(CLI_ID=clientid, SER_ID=serverid))
+        self._paths.append({clientid, serverid})
+
+    def path_exists(self, clientid, serverid):
+        if {clientid, serverid} in self._paths:
+            return True
+        else:
+            return False
+
+    def save(self):
+        command = ('$n commit\n'
+                   '$n save -name "{NAME}" -force')
+        self.tfiles.pcreate(command.format(NAME=self._name))
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ bpauto.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python3
+"""Library for generating BreakingPoint TCL commands."""
+
+from io import StringIO
+import yaml
+from itertools import cycle
+from bp import BreakingPoint
+
+class AutoNetwork(object):
+
+    def __init__(self, net_conf, bps):
+        self._net_conf = net_conf
+        self._network = bps.create_network(name=net_conf["Name"])
+        self._mac = self._gen_mac()
+
+    def save(self):
+        self._network.save()
+
+    def _gen_mac(self):
+        for i in range(197, 255):
+            for j in range(1, 255):
+                yield "02:1A:{:02x}:{:02x}:00:00".format(i, j)
+
+    def _get_ip_gen(self, ip_address, inc_mask):
+        def gen_ip_address1(template, start, inc):
+           for i in range(start, 255, inc):
+               yield template.format(i)
+
+        def gen_ip_address2(template, start1, start2, inc):
+           for i in range(start1, 255):
+               for j in range(start2, 255, inc):
+                   yield template.format(i, j)
+
+        octets = ip_address.split(sep='.')
+        mask = inc_mask.split(sep='.')
+        change_first = False
+        if mask[0] != '0':
+           start = octets[0]
+           inc = mask[0]
+           octets[0] = '{}'
+           change_first = True
+        elif mask[1] != '0':
+           start1 = octets[0]
+           start2 = octets[1]
+           inc = mask[1]
+           octets[0] = '{}'
+           octets[1] = '{}'
+        elif mask[2] !='0':
+           start1 = octets[1]
+           start2 = octets[2]
+           inc = mask[2]
+           octets[1] = '{}'
+           octets[2] = '{}'
+        elif mask[3] !='0':
+           raise ErrorError
+        template = '.'
+        template = template.join(octets)
+        if change_first:
+           return gen_ip_address1(template, int(start), int(inc))
+        else:
+           return gen_ip_address2(template, int(start1), int(start2), int(inc))
+
+    def generate_interfaces(self):
+       def gen_int_num(start, inc):
+           for i in range(start, 255, inc):
+               yield i
+
+       def gen_int_name(prefix, start, inc):
+           for i in range(start, 255, inc):
+               yield prefix + str(i)
+
+       int_mac = self._mac
+       for interface in self._net_conf["Interfaces"]:
+           int_num = gen_int_num(interface["Start Number"], interface["Increment"])
+           int_name = gen_int_name(interface["Name"], interface["Start Number"], interface["Increment"])
+           for _ in range(interface["Count"]):
+               self._network.add_interface(name=next(int_name), number=next(int_num), mac_address=next(int_mac))
+
+    def generate_vlans(self):
+        def gen_vlan_id(start, inc):
+            for i in range(start, 4096, inc):
+                yield i
+        
+        def gen_vlan_name(prefix, start, inc):
+           for i in range(start, 4096, inc):
+               yield prefix + str(i)
+
+        for vlan in self._net_conf['VLANs']:
+            vlan_name = gen_vlan_name(vlan['Name'], vlan['VLAN ID'], vlan['Increment'])
+            container = cycle(self._network.get_container_group(vlan["Container"]))
+            vlanid = gen_vlan_id(vlan['VLAN ID'], vlan['Increment'])
+            vlan_mac = self._mac
+            for _ in range(vlan['Count']):
+                self._network.add_vlan(name=next(vlan_name), container=next(container), vlan=next(vlanid), mac_address=next(vlan_mac))
+
+    def generate_ip_routers(self):
+        def gen_ip_router_name(prefix):
+           for i in range(1, 1000):
+               yield prefix + str(i)
+
+        for ip_routers in self._net_conf['IP Routers']:
+            ip_address = self._get_ip_gen(ip_routers['IP Address'], ip_routers['Increment Mask'])
+            gw_address = self._get_ip_gen(ip_routers['Gateway'], ip_routers['Increment Mask'])
+            ip_router_name = gen_ip_router_name(ip_routers['Name'])
+            container = cycle(self._network.get_container_group(ip_routers["Container"]))
+
+            for _ in range(ip_routers['Count']):
+                self._network.add_ip_router(name=next(ip_router_name), container=next(container),
+                                            ip_address=next(ip_address), gateway=next(gw_address),
+                                            netmask=ip_routers['Netmask'])
+
+    def generate_hosts(self):
+        def gen_hosts_name(prefix):
+           for i in range(1, 1000):
+               yield prefix + str(i)
+
+        for hosts in self._net_conf['IP Static Hosts']:
+           ip_address = self._get_ip_gen(hosts['IP Address'], hosts['Increment Mask'])
+           gw_address = self._get_ip_gen(hosts['Gateway'], hosts['Increment Mask'])
+           hosts_name = gen_hosts_name(hosts['Name'])
+           tag = hosts['Name']
+           container = cycle(self._network.get_container_group(hosts["Container"]))
+
+           for _ in range(hosts['Count']):
+               self._network.add_ip_static_hosts(name=next(hosts_name), tag=tag, container=next(container),
+                                           ip_address=next(ip_address), count=hosts['IP Count'], netmask=hosts['Netmask'],
+                                           gateway=next(gw_address))
+
+        for hosts in self._net_conf['IP Static Hosts']:
+            names = self._network.get_ip_static_hosts_group(hosts['Name'])
+            peernames = self._network.get_ip_static_hosts_group(hosts['Path'])
+            if len(names) > len(peernames):
+                peernames = cycle(peernames)
+            elif len(peernames) > len(names):
+                names = cycle(names)
+            for name, peer in zip(names, peernames):
+                if not self._network.path_exists(name, peer):
+                    self._network.add_path(name, peer)
+                
+
+
+
+class AutoBP(object):
+
+    def __init__(self, configfilename):
+        with open(configfilename) as configfile:
+            self._conf = yaml.load(configfile)
+        self._conn_conf = self._conf['Connection']
+        self._gen_conf = self._conf['General']
+        self._net_conf = self._conf['Network']
+
+        self._bps = BreakingPoint(prefix=self._gen_conf['Prefix'])
+        self._bps.connect(hostname=self._conn_conf['Tester IP'])
+
+    def generate_network(self):
+        self._network = AutoNetwork(self._net_conf, self._bps) 
+        return self._network
+
+    def save(self):
+        self._bps.save()
+
+def main():
+
+    autobp = AutoBP('conf.yaml')
+    network = autobp.generate_network()
+    network.generate_interfaces()
+    network.generate_vlans()
+    network.generate_ip_routers()
+    network.generate_hosts()
+    network.save()
+    autobp.save()
+
+
+if __name__ == "__main__":
+    main()
